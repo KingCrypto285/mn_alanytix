c713626aa7db9d69f2de5c51f9210ddd74a0f737
Ktech <54085500+KingCrypto285@users.noreply.github.com>
2024-02-27 23:00:57 +1300

    main
    x

---
+++ src/Pages/Pages.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {  Routes, Route } from "react-router-dom";
+import "../App.css";
+import Home from './HomePage';
+import Navbar from '../Components/Navbar';
+
+function Pages() {
+
+  
+    return (
+        <div>
+            <Navbar/>
+        </div>
+  );
+}
+
+export default Pages;
