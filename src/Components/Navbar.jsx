6433a40d738c91082deb38790ce35a07906a0f66
Ktech <54085500+KingCrypto285@users.noreply.github.com>
2024-03-03 22:29:59 +1300

    Revert "Report page work"

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,6 @@ import ReportSection from "../Pages/ReportSection";
 import ProjectSection from "../Pages/ProjectSection";
 import About from "../Pages/About";
 import Contact from "../Pages/Contact";
-import logo from "../Images/logo.png"
 
 const Navbar = () => {
 
